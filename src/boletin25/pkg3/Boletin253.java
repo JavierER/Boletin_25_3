a6e5c47c07e59238df97edf5e9cbccb6692d12e8
Usuario <Usuario@192.168.0.154>
2016-06-10 23:28:27 +0200

    Finalisado

---
+++ src/boletin25/pkg3/Boletin253.java
@@ -0,0 +1,15 @@
+package boletin25.pkg3;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Boletin253 {
+
+    
+    public static void main(String[] args) {
+        Frame window=new Frame();
+        window.setVisible(true);
+    }
+    
+}
